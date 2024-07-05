6172c863f501411e9a28f0c057d0c1c9125de978
arogya <arogyabichpuria16@gmail.com>
2024-07-05 09:28:49 +0530

    style updates

--- src/modules/loginForm/index.tsx
+++ src/modules/loginForm/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { TextInput } from "react-native";
-import { Stack, XStack, YStack, Button, Text, View, Input } from "tamagui";
+import { Stack, YStack, Button, Text, View } from "tamagui";
 import TextField from "../../components/TextField";
 import { Link } from "expo-router";
 import { colors } from "../../theme";
@@ -10,49 +9,32 @@ const LoginForm = () => {
     <Stack
       backgroundColor={colors.primaryBlack}
       justifyContent='center'
-      // borderWidth='1px'
-      // borderColor={colors.primaryWhite}
-      // borderStyle='solid'
       height='100%'
       alignItems='center'
       padding='$4'
     >
       <YStack
-        background='$background'
+        backgroundColor={colors.primaryWhite}
         borderRadius='$4'
         padding='$4'
         maxWidth='300px'
-        alignItems='center'
       >
-        <Text fontWeight='400' color='white'>
+        <Text fontWeight='400' >
           Welcome back
         </Text>
-        <Text fontWeight='400' color='white'>
-          Please enter your details to sign in.
-        </Text>
-        <YStack width='100px'>
-          <Text color={colors.primary}>Email</Text>
-          <TextField placeholder='Enter your email' />
+        <YStack  width='200'>
+          <Text padding='$0' color={colors.primary}>Email</Text>
+          <TextField margin="$0" width="200px" padding='$0' placeholder='Enter your email' />
         </YStack>
         <YStack>
           <Text color={colors.primary}>Password</Text>
           <TextField placeholder='Enter your password' secureTextEntry />
         </YStack>
-        <XStack
-          gap='$4'
-          alignItems='center'
-          justifyContent='space-between'
-          padding='$4'
-        >
-          <Button theme='link'>
-            <Text>Forgot password?</Text>
-          </Button>
-        </XStack>
         <View display='flex' gap='$4'>
           <Button theme='primary'>
             <Text>Sign in</Text>
           </Button>
-          <Button theme='link'>
+          <Button>
             <Link href='/signup'>Don't have an account yet? Sign Up</Link>
           </Button>
         </View>
