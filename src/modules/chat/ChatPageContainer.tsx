0526b75cd37e97752868762169118745514d6bce
Arogya Bichpuria <arogyabichpuria16@gmail.com>
2024-07-12 23:04:10 +0530

    added in props

--- src/modules/chat/ChatPageContainer.tsx
+++ src/modules/chat/ChatPageContainer.tsx
@@ -1,17 +1,21 @@
-import { Stack, Text } from "tamagui"
-import { colors } from "../../theme"
-import TextField from "../../components/TextField"
-import  ChatWidget from "./ChatWidget"
-
+import { Stack, Text } from "tamagui";
+import { colors } from "../../theme";
+import TextField from "../../components/TextField";
+import ChatWidget from "./ChatWidget";
 
 export const ChatPageContainer = () => {
-    return(
-        <Stack  height="100%" backgroundColor={colors.notQuiteBlack}>
-            <Stack>
-                <TextField borderRadius="$8" placeholder="Search..." />
-            </Stack>
+  return (
+    <Stack height="100%" backgroundColor={colors.notQuiteBlack}>
+      <Stack>
+        <TextField borderRadius="$8" placeholder="Search..." />
+      </Stack>
 
-            <ChatWidget />
-        </Stack>
-    )
-}
+      <ChatWidget
+        avatarUrl="https://example.com/avatar.jpg"
+        name="Mine"
+        message="https://youtu.be/rc_zVAS..."
+        timestamp="Yesterday"
+      />
+    </Stack>
+  );
+};
