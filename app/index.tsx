481ddc6dbb067bd3a9202961aea73112d6ab4cdb
Arogya Bichpuria <arogyabichpuria16@gmail.com>
2024-08-04 23:05:27 +0530

    initilized the queryClientProvider

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { createTamagui, TamaguiProvider } from "@tamagui/core";
 import { config } from "@tamagui/config/v3";
+import {QueryClientProvider, QueryClient} from "@tanstack/react-query"; 
 import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
 import LoginForm from "../src/modules/loginForm";
 import appConfig from "../tamagui.config";
 // import {customConfig} from "./theme"
 
 const tamaguiConfig = createTamagui({ ...config, appConfig });
-
+const queryClient = new QueryClient(); 
 export default function App() {
   let [fontsLoaded, fontError] = useFonts({
     Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
@@ -19,8 +20,10 @@ export default function App() {
   }
 
   return (
+    <QueryClientProvider client={queryClient}>
     <TamaguiProvider config={tamaguiConfig}>
       <LoginForm />
     </TamaguiProvider>
+    </QueryClientProvider>
   );
 }
